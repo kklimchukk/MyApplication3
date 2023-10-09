7e48c3396ebdd27e1e4d9acb2a78fb5e741b86f6
kklimchukk <kirilklim4uk@gmail.com>
2023-10-09 22:36:32 +0300

    added first button functionality

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myapplication3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button1);
+        TextView change_text = findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "wow, text changed", Toast.LENGTH_SHORT).show();
+                change_text.setText("HELLO FROM UKRAINE!!");
+            }
+        });
+
     }
+
 }
